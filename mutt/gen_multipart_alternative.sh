3dbeca15a65543d26857ceccd33e083abaef9ce5
Virginia Senioria <91khr@users.noreply.github.com>
2022-03-31 19:52:20 +0800

    feat mutt: pretty HTML, enchant reader

--- mutt/gen_multipart_alternative.sh
+++ mutt/gen_multipart_alternative.sh
@@ -5,7 +5,5 @@ path=$(dirname $(readlink -f $0))
 
 echo text/html
 echo
-sed -n -e "/$pattern/{x;p;x}; /$pattern/,\${p;b}" \
-    -e '/./{H;$!d};x; s/ \+\n> \+/ /g; s/ \+\n/ /g; p' | \
-    pandoc -s -f gfm+hard_line_breaks -t html --template ${path}/multipart_tmpl.html --metadata title=" "
+pandoc -s -f ${path}/mail_reader.lua -t html --mathml --template ${path}/multipart_tmpl.html --metadata title=" "
 
---
+++ mutt/mail_reader.lua
@@ -0,0 +1,28 @@
+-- Pandoc reader for pure text mails
+
+function Reader(input, opts)
+    -- Turn flow into line break
+    local src = tostring(input)
+    src = src:gsub(" \n", " \\\n")
+    -- Split out the responsing line
+    src = src:gsub("\n\nOn.+,.+wrote:\n", "%0\n")
+    -- Flip soft break and line break :) x
+    local filter = {
+        SoftBreak = function(s)
+            return pandoc.LineBreak {}
+        end,
+        LineBreak = function(s)
+            return pandoc.SoftBreak {}
+        end,
+    }
+    setmetatable(filter, {
+        __index = function(k)
+            return function(s)
+                flow = false
+                return s
+            end
+        end,
+    })
+    return pandoc.read(src, "gfm+raw_tex+tex_math_dollars"):walk(filter)
+end
+
