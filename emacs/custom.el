2ad14db0f3ff57c4fc1a5ee8305f7c800cccedf5
Virginia Senioria <91khr@users.noreply.github.com>
2022-08-11 18:11:22 +0800

    feat emacs/evil: set redo system

--- emacs/custom.el
+++ emacs/custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-undo-system 'undo-redo)
  '(inhibit-startup-screen t)
  '(make-backup-files nil)
  '(markdown-enable-math t)
