51f92a01ce435e19ed498d3251c0dbb799f0eb90
Virginia Senioria <91khr@users.noreply.github.com>
2022-08-30 09:42:34 +0800

    feat utils/puncttrans: all zh support

--- utils/puncttrans.lua
+++ utils/puncttrans.lua
@@ -1,45 +1,47 @@
 #!/bin/env lua
 
-function split_str(str)
-    return function(str, pos)
-        str = str:sub(pos)
-        if str:len() == 0 then return nil end
+local all_zh = os.getenv("ALL_ZH") == "1"
+
+local function split_str(str)
+    return function(cur, pos)
+        cur = cur:sub(pos)
+        if cur:len() == 0 then return nil end
         -- BBCode tag or link
-        local prefix = str:match("^!?%[.-%]")
+        local prefix = cur:match("^!?%[.-%]")
         if prefix then
-            local peek = str:match("^%(.-%)", prefix:len() + 1)
+            local peek = cur:match("^%(.-%)", prefix:len() + 1)
             if peek then prefix = prefix .. peek end
             return pos + prefix:len(), prefix, "tag/link"
         end
         -- link definition
-        prefix = str:match("^\n%[[^\n]-%]:[^\n]*\n")
+        prefix = cur:match("^\n%[[^\n]-%]:[^\n]*\n")
         if prefix then
             return pos + prefix:len(), prefix, "linkdef"
         end
         -- Code (inline or block)
-        prefix = str:match("^`+")
+        prefix = cur:match("^`+")
         if prefix then
-            local _1, peek = str:find(string.rep("`", prefix:len()), prefix:len() + 1, true)
-            if not peek then peek = str:len() end
-            return pos + peek, str:sub(1, peek), "code"
+            local _1, peek = cur:find(string.rep("`", prefix:len()), prefix:len() + 1, true)
+            if not peek then peek = cur:len() end
+            return pos + peek, cur:sub(1, peek), "code"
         end
         -- Ordered lists
-        prefix = str:match("^\n[ \t]-%d+%.")
+        prefix = cur:match("^\n[ \t]-%d+%.")
         if prefix then
             return pos + prefix:len(), prefix, "ordlist"
         end
         -- Normal text
-        local peek = str:find("[%[!`\n\\]", str:match("^\\") and 3 or (str:match("^[!\n]") and 2 or 1))
-        if not peek then peek = str:len() + 1 end
-        return pos + peek - 1, str:sub(1, peek - 1), "ordinary"
+        local peek = cur:find("[%[!`\n\\]", cur:match("^\\") and 3 or (cur:match("^[!\n]") and 2 or 1))
+        if not peek then peek = cur:len() + 1 end
+        return pos + peek - 1, cur:sub(1, peek - 1), "ordinary"
     end, str, 1
 end
 
-lastlang = "zh"
-quotes = {}
-function replace_punct(str)
+local lastlang = "zh"
+local quotes = {}
+local function replace_punct(str)
     local res = str:gsub("([^%p%d%s]?)([().,:;?!\"' \t]+)", function(lead, punct)
-        if lead:match("%a") or (lead == "" and lastlang == "en") then
+        if not all_zh and (lead:match("%a") or (lead == "" and lastlang == "en")) then
             lastlang = "en"
         elseif lead:len() > 0 then
             lastlang = "zh"
@@ -90,7 +92,7 @@ function replace_punct(str)
     return res
 end
 
-src = io.stdin:read("*all")
+local src = io.stdin:read("*all")
 for pos, ctnt, needy in split_str(src) do
     if needy == "ordinary" then
         ctnt = replace_punct(ctnt)
