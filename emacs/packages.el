d186f5ce52e2ea89bb0fa66fbe1446f69968523c
Virginia Senioria <91khr@users.noreply.github.com>
2022-09-26 12:16:06 +0800

    refa emacs: update selected packages, reindent packages.el

--- emacs/packages.el
+++ emacs/packages.el
@@ -2,7 +2,7 @@
 (require 'package)
 (setq package-archives
       '(("melpa" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-        ))
+        ("elpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
         ;;("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 (when (not (boundp 'package-list))
@@ -24,18 +24,18 @@
 
 ;; Individual package config
 (use-package markdown-mode
-             :ensure t
-             :mode (("README\\.md\\'" . gfm-mode)
-                    ("\\.md\\'" . markdown-mode)
-                    ("\\.markdown\\'" . markdown-mode))
-             :config (load-file (concat dotdir "pkgconf/markdown.el")))
+  :ensure t
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :config (load-file (concat dotdir "pkgconf/markdown.el")))
 
 (use-package evil
-             :config (load-file (concat dotdir "pkgconf/evil.el")))
+  :config (load-file (concat dotdir "pkgconf/evil.el")))
 
 (use-package meow
-             :config (load-file (concat dotdir "pkgconf/meow.el")))
+  :config (load-file (concat dotdir "pkgconf/meow.el")))
 
 (use-package racket-mode
-             :config (load-file (concat dotdir "pkgconf/racket.el")))
+  :config (load-file (concat dotdir "pkgconf/racket.el")))
 
