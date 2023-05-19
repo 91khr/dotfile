1b9fb386fc574c4ddde32c9d4dc5d4e62230573b
Virginia Senioria <91khr@users.noreply.github.com>
2023-05-19 10:21:11 +0800

    feat mutt: tweak muttrc for keybind etc.
    (Preparing to switch to emacs/vim... x x)

--- mutt/detect_localrc.sh
+++ mutt/detect_localrc.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 
 [ -z "$LOCAL" ] || exit
-find ~/.mutt -type f -name '*.muttrc' -printf "source %p\nfolder-hook \$folder \"source %p\"\n"
+for f in $(find ~/.mutt -type f -name '*.muttrc'); do
+    echo "source $f"
+    echo "folder-hook \$folder \"source $f\""
+done
 test -f ~/.mutt/main.muttrc && echo 'set folder=$my_main_folder'
 
