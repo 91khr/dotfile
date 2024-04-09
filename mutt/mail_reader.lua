b9e5a23409789f1695cef5dfdb8edbfb230505c7
Virginia Senioria <91khr@users.noreply.github.com>
2024-04-09 20:38:41 +0800

    misc profile/clang-format,emacs,mutt:
    tweak clang-format for newline escape alignment
    stop dropping responsing line
    use emacs for matrix (???)

--- mutt/mail_reader.lua
+++ mutt/mail_reader.lua
@@ -5,7 +5,7 @@ function Reader(input, opts)
     local src = tostring(input)
     src = src:gsub(" \n", " \\\n")
     -- Split out the responsing line
-    src = src:gsub("\n\nOn.+,.+wrote:\n", "%0\n")
+    --src = src:gsub("\n\nOn.+,.+wrote:\n", "%0\n")
     -- Flip soft break and line break :) x
     local filter = {
         SoftBreak = function(s)
@@ -15,13 +15,6 @@ function Reader(input, opts)
             return pandoc.SoftBreak {}
         end,
     }
-    setmetatable(filter, {
-        __index = function(k)
-            return function(s)
-                return s
-            end
-        end,
-    })
     return pandoc.read(src, "gfm+tex_math_dollars"):walk(filter)
 end
 
