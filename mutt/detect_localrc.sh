936fee976f5cf1a0a19815d3e29b3c606bf35e11
Virginia Senioria <91khr@users.noreply.github.com>
2025-01-12 18:24:02 +0800

    feat mutt: support oauth

--- mutt/detect_localrc.sh
+++ mutt/detect_localrc.sh
@@ -3,7 +3,7 @@
 [ -z "$LOCAL" ] || exit
 for f in $(find ~/.mutt -type f -name '*.muttrc'); do
     echo "source $f"
-    echo "folder-hook \$folder \"source $f\""
+    echo "account-hook \$folder \"unset imap_oauth_refresh_command smtp_oauth_refresh_command ; source $f\""
 done
 test -f ~/.mutt/main.muttrc && echo 'set folder=$my_main_folder'
 
